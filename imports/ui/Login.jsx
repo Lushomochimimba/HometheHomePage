c16c6b1dc5fa61e469a5b446423a23d14bf77338
Perseverance23 <persymila@gmail.com>
2018-05-11 16:09:06 +0200

    added sign up and login components

--- imports/ui/Login.jsx
+++ imports/ui/Login.jsx
@@ -4,11 +4,26 @@ import route from '/imports/routing/router.js';
 
 export default class Login extends Component{
 
-   
+
     render(){
         return(
             <div>
-                <h2>Login</h2>
+                <h1>Log In</h1>
+                <div className="SignUp">
+        <form className="col s12">
+          <div className="row">
+            <div className="input-field col s12">
+              <input placeholder="Placeholder" id="first_name" type="text" className="validate"/>
+              <label type="first_name">First Name</label>
+            </div>
+            <div className="input-field col s12">
+              <input id="last_name" type="text" className="validate"/>
+              <label type="last_name">Last Name</label>
+            </div>
+          </div>
+          </form>
+          <button type="Primary" className="btn btn-primary">Sign In</button>
+            </div>
             </div>
         )
     }
--- imports/ui/SignUp.jsx
+++ imports/ui/SignUp.jsx
@@ -4,12 +4,53 @@ import route from '/imports/routing/router.js';
 
 export default class SignUp extends Component{
 
-    
+
     render(){
         return(
             <div>
-                <h2>SignUp</h2>
-            </div>
+                <h1>Sign Up</h1>
+            <div className="SignUp">
+    <form className="col s12">
+      <div className="row">
+        <div className="input-field col s12">
+          <input placeholder="Placeholder" id="first_name" type="text" className="validate"/>
+          <label type="first_name">First Name</label>
+        </div>
+        <div className="input-field col s12">
+          <input id="last_name" type="text" className="validate"/>
+          <label type="last_name">Last Name</label>
+        </div>
+      </div>
+      <div className="row">
+        <div className="input-field col s12">
+          <input id="email" type="email" className="validate"/>
+          <label type="email">Email</label>
+        </div>
+      </div>
+      <div className="row">
+        <div className="input-field col s12">
+          <input id="password" type="password" className="validate"/>
+          <label type="password">Create Password</label>
+        </div>
+      </div>
+      <div className="row">
+        <div className="input-field col s12">
+          <input id="password" type="password" className="validate"/>
+          <label type="password">Confirm Password</label>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col s12">
+          <div className="input-field inline">
+
+          </div>
+        </div>
+      </div>
+    </form>
+    <button type="Primary" className="btn btn-primary">Sign Up</button>
+    <p>Already have an account?<a href="#">Sign In</a></p>
+  </div>
+  </div>
         )
     }
 }
